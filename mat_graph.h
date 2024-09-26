b504457f7317548c1360336f319ab324d8de2e35
Yesihang <1148131699@qq.com>
2024-09-26 22:23:04 +0800

    Create mat_graph.h

---
+++ mat_graph.h
@@ -0,0 +1,138 @@
+//邻接矩阵----------------------------------------------------------------
+#pragma once
+#include<stdio.h>
+#include<stdlib.h>
+#define NUM 20
+
+typedef int elemtype;
+typedef int weight; //权
+typedef struct {
+	elemtype vertex[NUM];
+	int arc[NUM][NUM];
+	int numnode, numedge; //顶点数，边数
+}AMgraph;
+
+void CreateMGraph(AMgraph* G)             //创建邻接矩阵（无权无向）
+{
+	int n, m;
+	elemtype date;
+	printf("请输入顶点数和边数：\n");
+	scanf_s("%d %d", &G->numnode, &G->numedge);
+	printf("请依次输入顶点数据：\n");
+	for (int a = 0; a < G->numnode; a++)
+	{
+		printf("->");
+		scanf_s("%d", &date);
+		G->vertex[a] = date;
+	}
+	for (int c = 0; c < NUM; c++)
+		for (int d = 0; d < NUM; d++)
+			G->arc[c][d] = 0;
+	printf("请输入(v,v')的依附顶点下标：\n");
+	for (int b = 0; b < G->numedge; b++)
+	{
+		printf("->");
+		scanf_s("%d  %d", &n, &m);
+		G->arc[n][m] = 1;
+		G->arc[m][n] = 1;
+	}
+}
+
+
+_Bool visited[NUM];
+
+void DFS(AMgraph* G,int i)         //邻接矩阵的深度优先递归算法
+{
+	visited[i] = 1;
+	printf("->%d", G->vertex[i]);
+
+	for (int j = 0; j < G->numnode; j++)
+		if (G->arc[i][j] == 1 && !visited[j])
+			DFS(G, j);
+}
+
+void DFSTraverse(AMgraph* G)          //邻接矩阵的深度遍历
+{
+	int i;
+	for (i = 0; i < G->numnode; i++)
+		visited[i] = 0;
+	for (i = 0; i < G->numnode; i++)
+		if (!visited[i])
+			DFS(G, i);
+}
+
+
+//辅助队列--------------------------------------------------------------
+typedef int elemtype;         
+typedef struct {
+	elemtype date[NUM];
+	int front;
+	int rear;
+}cirQueue;
+typedef cirQueue* poin_queue;
+int InitQueue(poin_queue Q)           //初始化
+{
+	Q->front = 0;
+	Q->rear = 0;
+	return 0;
+}
+int EnQueue(poin_queue Q, elemtype e)          //入队
+{
+	if ((Q->rear + 1) % NUM == Q->front)
+	{
+		printf("队列已满！");
+		return 0;
+	}
+	Q->date[Q->rear] = e;
+	Q->rear = (Q->rear + 1) % NUM;
+	return 0;
+}
+int DeQueue(poin_queue Q, elemtype* e)         //出队，并将值赋给e
+{
+	if (Q->front == Q->rear)
+	{
+		printf("这是一个空队列！");
+		return 0;
+	}
+	*e = Q->date[Q->front];
+	Q->front = (Q->front + 1) % NUM;
+	return 0;
+}
+int QueueEmpty(poin_queue Q)         //检查是否为空
+{
+	if (Q->front == Q->rear)
+	    return 1;
+	else
+		return 0;
+}
+//辅助队列---------------------------------------------------
+
+void BFSTraverse(AMgraph* G)          //邻接矩阵广度遍历算法
+{
+	cirQueue Q;
+	int i, j;
+	for (i = 0; i < G->numnode; i++)
+		visited[i] = 0;
+	InitQueue(&Q);
+	for (i = 0; i < G->numnode; i++)
+	{
+		if (visited[i] == 0)
+		{
+			visited[i] = 1;
+			printf("->%d", G->vertex[i]);
+			EnQueue(&Q, i);
+			while (QueueEmpty(&Q) == 0)
+			{
+				DeQueue(&Q, &i);
+				for (j = 0; j < G->numnode; j++)
+					if (G->arc[i][j] == 1 && visited[j] == 0)
+					{
+						visited[j] = 1;
+						printf("->%d", G->vertex[j]);
+						EnQueue(&Q, j);
+					}
+			}
+		}
+	}
+
+}
